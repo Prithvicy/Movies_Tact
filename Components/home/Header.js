948fde7d8273595cb439402fcd019fd28290da2c
Prith Kami <prithvi40201@gmail.com>
2022-05-19 13:12:40 +0530

    header compoenent

--- Components/home/Header.js
+++ Components/home/Header.js
@@ -1,6 +1,9 @@
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import React from "react";
-
+import { Feather } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import Position from "react-native/Libraries/Components/Touchable/Position";
 export default function Header() {
   return (
     <View style={styles.container}>
@@ -11,9 +14,41 @@ export default function Header() {
         />
       </TouchableOpacity>
       <View style={styles.iconsContainer}>
-        <Text style={{ color: "red" }}>heelo</Text>
-        <Text style={{ color: "red" }}>heelo</Text>
-        <Text style={{ color: "red" }}>heel00</Text>
+        <TouchableOpacity>
+          <Image
+            style={{
+              borderColor: "red",
+            }}
+            source={require("../../assets/homelogo.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Feather
+            style={styles.icon}
+            name="plus-square"
+            size={24}
+            color="red"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Entypo
+            style={styles.icon}
+            name="heart-outlined"
+            size={24}
+            color="red"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.unreadBadge}>
+            <Text style={styles.unreadBadgeText}>11</Text>
+          </View>
+          <FontAwesome
+            style={styles.icon}
+            name="send-o"
+            size={24}
+            color="red"
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -33,4 +68,26 @@ const styles = StyleSheet.create({
   iconsContainer: {
     flexDirection: "row",
   },
+  icon: {
+    width: 30,
+    color: "red",
+    height: 30,
+    marginLeft: 10,
+  },
+  unreadBadge: {
+    backgroundColor: "#B6FFCE",
+    position: "absolute",
+    top: -10,
+    right: 15,
+    alignItems: "center",
+    height: 18,
+    width: 25,
+
+    borderRadius: 50,
+    zIndex:10,
+  },
+  unreadBadgeText:{
+      color:"black",
+      fontWeight:"600",
+  }
 });
